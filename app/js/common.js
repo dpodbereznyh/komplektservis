e5bddda57644607e2d080f2837a7362d0e8dbc0a
dpodbereznyh <dpodbereznyh@gmail.com>
2020-11-27 13:29:41 +0500

    Final

--- app/js/common.js
+++ app/js/common.js
@@ -48,16 +48,48 @@ $(document).ready(function () {
     });
 
     //Accorion
-    $('.answers__accordion_btn').on('click', function() {
-        // $('.answers__accordion_btn').removeClass('answers-active');
-        $(this).toggleClass('answers-active');
-    });
-    $('.answers__accordion_btn_hide').on('click', function() {
-        $('.answers__accordion_btn').removeClass('answers-active');
-        $(this).toggleClass('answers-active');
-    });
+    // $('.answers__accordion_btn').on('click', function() {
+    //     // $('.answers__accordion_btn').removeClass('answers-active');
+    //     $(this).toggleClass('answers-active');
+    // });
+    // $('.answers__accordion_btn_hide').on('click', function() {
+    //     $('.answers__accordion_btn').removeClass('answers-active');
+    //     $(this).toggleClass('answers-active');
+    // });
     // $('.answers__accordion_btn_hide').on('click', function(e) {
     //     e.preventDefault;
     //     $(this).toggleClass('answers-active');
     // });
 });
+const accordion = (triggersSelector) => {
+    const accordionBtn = document.querySelectorAll(triggersSelector);
+    accordionBtn.forEach(btn => {
+        btn.addEventListener('click', function() {
+            if(!this.classList.contains('answers-active')) {
+                accordionBtn.forEach(btn => {
+                    btn.classList.remove('answers-active');
+                });
+                this.classList.add('answers-active');
+            } else {
+                this.classList.remove('answers-active');
+            }
+        });
+    });
+     
+};
+const accordionClose = (triggerSelector) => {
+    const accordionBtnHide = document.querySelectorAll(triggerSelector); 
+    const accordionBtnActive = document.querySelectorAll('.answers__accordion_btn');   
+    accordionBtnHide.forEach(item => {
+        item.addEventListener('click', function() {
+            accordionBtnActive.forEach(btn => {
+                if(btn.classList.contains('answers-active')) {
+                    btn.classList.remove('answers-active');
+                }
+            });
+            
+        });
+    });
+};
+accordionClose('.answers__accordion_btn_hide');
+accordion('.answers__accordion_btn');
